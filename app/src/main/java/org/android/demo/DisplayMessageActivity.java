d32e74876288ef70b3f0f5c8a31c5df18ff10580
Viacheslav Rarata <cayneir@gmail.com>
2017-03-22 16:48:28 +0200

    created new fragment and added to  Activity

--- app/src/main/java/org/android/demo/DisplayMessageActivity.java
+++ app/src/main/java/org/android/demo/DisplayMessageActivity.java
@@ -7,18 +7,25 @@ import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    private static final String DEFAULT_MESSAGE = "You didn't typed nothing!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message = getMessage(intent);
 
-        // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(message);
+    }
+
+    private String getMessage(Intent intent) {
+        return !empty(intent.getStringExtra(MainActivity.EXTRA_MESSAGE)) ? intent.getStringExtra(MainActivity.EXTRA_MESSAGE) : DEFAULT_MESSAGE;
+    }
+
+    private boolean empty(String text) {
+        return (null != text && !"".equals(text)) ? Boolean.FALSE : Boolean.TRUE;
     }
 }
--- app/src/main/java/org/android/demo/MainActivity.java
+++ app/src/main/java/org/android/demo/MainActivity.java
@@ -3,6 +3,7 @@ package org.android.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Get a string resource from your app's Resources
-        String hello = getResources().getString(R.string.hello_android);
+        String hello_android = getResources().getString(R.string.hello_android);
+        Log.v("hello_android", hello_android);
 
         TextView textView = new TextView(this);
         textView.setText(R.string.hello_android);
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void navigateToFragment(View view) {
+        Intent intent = new Intent(this, org.android.demo.fragments.MainActivity.class);
         startActivity(intent);
     }
 }
